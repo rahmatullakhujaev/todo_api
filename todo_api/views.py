2b2ea86f711e3ff190c0de5fb91bf248ce62c4d1
rahmatullakhujaev <r.xo'jayev@student.inha.uz>
2024-04-22 22:21:26 +0500

    changes added to ApiView post nas ViewSet

--- todo_api/views.py
+++ todo_api/views.py
@@ -22,8 +22,9 @@ class TodoLisApiView(APIView):
 
 
     def post(self, request, *args, **kwargs):
-        serializer = TodoSerializers(data=request.data)
+        serializer = TodoSerializer(data=request.data)
         if serializer.is_valid():
+            serializer.validated_data['user'] = request.user
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -62,6 +63,8 @@ class TodoDetailApiView(APIView):
         return Response({"message": "Todo succesfully deleted!"})
 
 class TodoViewSet(ViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsOwner]
     queryset = Todo.objects.all()
 
     def list(self, request, *args,**kwargs):
